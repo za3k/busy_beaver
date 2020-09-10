4427523eb82e5daec50266e03b3369702e296cfd
Zachary Vance <za3k@za3k.com>
2020-09-10 05:12:50 -0700

    Get program working by fixing highest_state

--- lazy_beaver.py
+++ lazy_beaver.py
@@ -3,7 +3,6 @@ from collections import defaultdict
 
 DIRECTIONS = (-1, 1)
 SYMBOLS = (0, 1)
-MAX_STEPS=1000
 
 class TuringMachine():
     def __init__(self, size):
@@ -11,13 +10,11 @@ class TuringMachine():
         self.size = size
 
         self.highest_state = 0
-        self.transition_initialized = False # premature optimization, can divide number of machines by 2 if we assume the first transition is to the right=1
 
-        self.write = {}
-        self.direction = {}
         self.transition_matrix = {}
         self.tape = defaultdict(int) # two-directional tape initialized to zeros
         self.position = 0
+
     def step(self):
         """Returns any number of new machines >= 1, and the original should be discarded"""
         machines = self.pre_split()
@@ -28,13 +25,14 @@ class TuringMachine():
     def concrete_step(self):
         if self.is_halted():
             return
+        #print(self.position, self.state, self.transition_matrix, self.tape)
         transition = self.transition()
         #print (self, transition, self.position, self.tape, self.transition_matrix)
-        self.state = self.transition_matrix[transition]
+        self.state, symbol, move = self.transition_matrix[transition]
         if self.is_halted():
             return
-        self.tape[self.position] = self.write[transition]
-        self.position += self.direction[transition]
+        self.tape[self.position] = symbol
+        self.position += move
 
     def transition(self):
         return (self.state, self.tape.get(self.position, 0))
@@ -47,10 +45,11 @@ class TuringMachine():
         split = []
         split.append(self.split(transition, write=None, direction=None, new_state=None))
         for state in range(0, min(self.highest_state + 2, self.size)):
-            if self.transition_initialized:
-                poss = DIRECTIONS
-            else:
+            poss = DIRECTIONS
+            if len(self.transition_matrix) == 0:
                 poss = (1,)
+            else:
+                poss = DIRECTIONS
             for direction in poss:
                 for symbol in SYMBOLS:
                     split.append(self.split(transition, write=symbol, direction=direction, new_state=state))
@@ -61,10 +60,10 @@ class TuringMachine():
         #print(transition, new_state, write, direction)
         assert (new_state is None and write is None and direction is None) or (0 <= new_state <= self.highest_state + 1 and new_state <= self.size - 1 and write in SYMBOLS and direction in DIRECTIONS)
         new = copy.deepcopy(self)
-        new.write[transition] = write
-        new.direction[transition] = direction
-        new.transition_matrix[transition] = new_state
         new.transition_initialized = True
+        new.transition_matrix[transition] = (new_state, write, direction)
+        if new_state is not None:
+            new.highest_state = max(new.highest_state, new_state)
         return new
 
     def is_halted(self):
@@ -73,12 +72,33 @@ class TuringMachine():
 def step_all(machines):
     return [x for machine in machines for x in machine.step()]
 
-machines = [TuringMachine(1)]
-for step_number in range(1, MAX_STEPS+1):
-    stepped_machines = step_all(machines)
-    running_machines = [machine for machine in stepped_machines if not machine.is_halted()]
-    num_halted = len(stepped_machines) - len(running_machines)
-    if num_halted != 0:
-        print("Halting on step {}: {} machines".format(step_number, num_halted))
-    machines = running_machines
-print("Didn't halt in {} steps: {} machines".format(MAX_STEPS, len(machines)))
+def distribution():
+    MACHINE_SIZE=4
+    MAX_STEPS=200
+
+    machines = [TuringMachine(MACHINE_SIZE)]
+    for step_number in range(1, MAX_STEPS+1):
+        stepped_machines = step_all(machines)
+        running_machines = [machine for machine in stepped_machines if not machine.is_halted()]
+        num_halted = len(stepped_machines) - len(running_machines)
+        if num_halted != 0:
+            print("Halting on step {}: {} machines".format(step_number, num_halted))
+        machines = running_machines
+    print("Didn't halt in {} steps: {} machines".format(MAX_STEPS, len(machines)))
+
+def lazy_beaver():
+    MACHINE_SIZE=4
+    MAX_STEPS=200
+
+    machines = [TuringMachine(MACHINE_SIZE)]
+    for step_number in range(1, MAX_STEPS+1):
+        print("Searching step {} ({}-state machines: {})".format(step_number, MACHINE_SIZE, len(machines)))
+        stepped_machines = step_all(machines)
+        running_machines = [machine for machine in stepped_machines if not machine.is_halted()]
+        num_halted = len(stepped_machines) - len(running_machines)
+        machines = running_machines
+        if num_halted == 0:
+            print("LB({})={}".format(MACHINE_SIZE, step_number))
+            return
+
+lazy_beaver()
